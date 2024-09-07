78c7460a0b8ff051724e07ad78a6ca89a57d6e10
Aarya Soni <155264128+AaryaSoni-web@users.noreply.github.com>
2024-09-07 20:32:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+
+document.addEventListener("keydown",function(event){
+    sound(event.key);
+    animation(event.key);
+        })
+for (var i=0;i<7;i++){
+    document.querySelectorAll("button")[i].addEventListener("click",function (){
+        sound(this.innerHTML);
+        animation(this.innerHTML);
+    })
+}
+function sound(btn){
+    switch(btn){
+        case "w":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+} 
+}
+function animation(btn){
+    document.querySelector("."+btn).classList.add("pressed");
+    setTimeout(function (){document.querySelector("."+btn).classList.remove("pressed");},100);
+
+
+}
